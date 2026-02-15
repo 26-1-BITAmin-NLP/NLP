6d520277b367df4dea5cb5cfbe2256b302587a46
림림 <hyerim23@ewhain.net>
2026-02-15 23:21:43 +0900

    feat: api 호출

---
+++ src/finance_agent/api/finance_api_utils.py
@@ -0,0 +1,29 @@
+import requests
+import json
+import os
+
+API_KEY = "01dea4cf112f940416c0c47b8b30a6da"
+
+BASE_URL = "http://finlife.fss.or.kr/finlifeapi"
+
+# API 호출
+def call_api(endpoint, params):
+    URL = BASE_URL + endpoint
+
+    params["auth"] = API_KEY
+
+    response = requests.get(URL, params=params)
+    data = response.json()
+    print("API 호출 완료")
+
+    return data["result"]
+
+
+# API 호출로 얻은 원본 데이터 저장
+def save_raw_json(data, filename):
+    os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+    print("raw data 저장 완료")
